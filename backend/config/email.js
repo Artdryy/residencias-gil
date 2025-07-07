45dc66341f78ba7f59e3cd753976a976c02cb6c7
Jorge Adrian Perez Dominguez <adriandominguez.contacto@gmail.com>
2025-07-07 14:43:33 -0600

    -- no ps

--- backend/config/email.js
+++ backend/config/email.js
@@ -4,7 +4,7 @@ const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: 'alu.22130593@correo.itlalaguna.edu.mx', 
-    pass: 'U1tima_GITO.Adry@@2025' 
+    pass: '' 
   }
 });
 
